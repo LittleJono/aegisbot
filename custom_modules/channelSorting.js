6f5bbc93d1f5b40cea58113b050de3d934c7699b
Jono Templeton <jono@wifi-172-23-0-145.uoa-wifi.auckland.ac.nz>
2018-07-17 10:13:01 +1200

    Playing with linter

--- custom_modules/channelSorting.js
+++ custom_modules/channelSorting.js
@@ -6,74 +6,74 @@ var guildID = config.guildID;
 var logger = require("../functions/logger");
 
 var path = require('path');
-path = path.basename(__filename)
+path = path.basename(__filename);
 
 
 var options = {
-    method: 'GET',
-    uri: 'https://discordapp.com/api/guilds/' + guildID + '/channels',
-    headers: {
-        'Authorization': 'Bot ' + token,
-        'User-Agent': 'DiscordBot (*, *)'
-    }
+	method: 'GET',
+	uri: 'https://discordapp.com/api/guilds/' + guildID + '/channels',
+	headers: {
+		'Authorization': 'Bot ' + token,
+		'User-Agent': 'DiscordBot (*, *)'
+	}
 };
 
 var channelSorting = {
-    loadModule: (client) => {
-        client.on("message", (message) => {
-            try {
-            var messageParams = message.content.toLowerCase().split(" ");
-            if (!message.guild.member(message.author.id).permissions.has('ADMINISTRATOR')) {
-                return;
-            } else if (messageParams[0] == ".sortchannels") {
-                var parentChannelID = messageParams[1];
-                rp(options)
-                    .then(function(body) {
-                        var childChannels = {};
-                        var childChannelNames = [];
-                        output = JSON.parse(body);
-                        //console.log(JSON.stringify(output, null, 4))
-                        var guildChannels = client.guilds.get(guildID).channels;
+	loadModule: (client) => {
+		client.on("message", (message) => {
+			try {
+			var messageParams = message.content.toLowerCase().split(" ");
+			if (!message.guild.member(message.author.id).permissions.has('ADMINISTRATOR')) {
+				return;
+			} else if (messageParams[0] == ".sortchannels") {
+				var parentChannelID = messageParams[1];
+				rp(options)
+					.then(function(body) {
+						var childChannels = {};
+						var childChannelNames = [];
+						output = JSON.parse(body);
+						//console.log(JSON.stringify(output, null, 4))
+						var guildChannels = client.guilds.get(guildID).channels;
 
-                        for (channel in output) {
-                            if (output[channel].parent_id == parentChannelID) {
-                                childChannelNames.push(output[channel].name)
-                                childChannels[output[channel].name] = {
-                                    "id": output[channel].id,
-                                    "position": output[channel].position
-                                }
-                            }
-                        }
-                        if (childChannelNames.length == 0) {
-                            message.channel.send("Cannot find channel with that ID.");
-                            logger.log("Channel with ID " + parentChannelID + " not found.", path);
-                            return;
-                        } else {
-                            childChannelNames = childChannelNames.sort()
-                            var count = 1;
-                            for (name in childChannelNames) {
-                                actualName = childChannelNames[name]
-                                logger.log('Sorting #' + actualName + ', Position: ' + count, path);
-                                var channel = guildChannels.get(childChannels[actualName].id)
-                                channel.edit({
-                                    position: count,
-                                    bitrate: 96000
-                                })
-                                count += 1;
-                            }
-                            message.channel.send("Channels sorted.");
-                            logger.log("Channels sorted.", path);
-                        }
-                    })
-                    .catch(function(err) {
-                        logger.log(err, path);
-                    });
-            }
-        } catch (err) {
-            logger.log(err, path);
-        }
-        })
-    }
-}
+						for (channel in output) {
+							if (output[channel].parent_id == parentChannelID) {
+								childChannelNames.push(output[channel].name)
+								childChannels[output[channel].name] = {
+									"id": output[channel].id,
+									"position": output[channel].position
+								}
+							}
+						}
+						if (childChannelNames.length == 0) {
+							message.channel.send("Cannot find channel with that ID.");
+							logger.log("Channel with ID " + parentChannelID + " not found.", path);
+							return;
+						} else {
+							childChannelNames = childChannelNames.sort();
+							var count = 1;
+							for (name in childChannelNames) {
+								var actualName = childChannelNames[name];
+								logger.log('Sorting #' + actualName + ', Position: ' + count, path);
+								var channel = guildChannels.get(childChannels[actualName].id);
+								channel.edit({
+									position: count,
+									bitrate: 96000
+								});
+								count += 1;
+							}
+							message.channel.send("Channels sorted.");
+							logger.log("Channels sorted.", path);
+						}
+					})
+					.catch(function(err) {
+						logger.log(err, path);
+					});
+			}
+		} catch (err) {
+			logger.log(err, path);
+		}
+		});
+	}
+};
 
 module.exports = channelSorting;
