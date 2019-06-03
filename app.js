9cb72f01ee2e1c22c83fadc856506495ca105808
LittleJono <jtem977@aucklanduni.ac.nz>
2019-06-03 17:50:09 +1200

    q

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 
 const fs = require('fs');
-let path = require('path');
+const _ = require('lodash');
 const Discord = require('discord.js');
-
-const config = require('./config/config'); // Load the configuration from config.js
-const notify = require('./custom_modules/notify');
-const antiWord = require('./custom_modules/antiWord');
-const getRoleIDs = require('./custom_modules/getRoleIDs');
-const autoRoles = require('./custom_modules/autoRoles');
-const channelRenaming = require('./custom_modules/channelRenaming');
-const channelSorting = require('./custom_modules/channelSorting');
-const unusedChannels = require('./custom_modules/unusedChannels.js');
-const getWammed = require('./custom_modules/getWammed.js');
-const roleAssignment = require('./custom_modules/roleAssignment.js');
-const logger = require('./functions/logger');
-
+let path = require('path');
 path = path.basename(__filename);
+
+
+const logger = require('./functions/logger');
+const config = require('./config/config'); // Load the configuration from config.js
+
 
 if (!fs.existsSync('logFiles')) {
   fs.mkdirSync('logFiles');
@@ -25,21 +18,48 @@ if (!fs.existsSync('cacheFiles')) {
   fs.mkdirSync('cacheFiles');
 }
 
+const modules = {
+  notify: {
+    enabled: config.notify,
+    file: require('./custom_modules/notify'),
+  },
+  getRoleIDs: {
+    enabled: config.getRoleIDs,
+    file: require('./custom_modules/getRoleIDs'),
+  },
+  antiWord: {
+    enabled: config.antiWord,
+    file: require('./custom_modules/antiWord'),
+  },
+  autoRoles: {
+    enabled: config.autoRoles,
+    file: require('./custom_modules/autoRoles'),
+  },
+  channelRenaming: {
+    enabled: config.channeRenaming,
+    file: require('./custom_modules/channelRenaming'),
+  },
+  channelSorting: {
+    enabled: config.channelSorting,
+    file: require('./custom_modules/channelSorting'),
+  },
+  unusedChannels: {
+    enabled: config.unusedChannels,
+    file: require('./custom_modules/unusedChannels.js'),
+  },
+  getWammed: {
+    enabled: config.getWammed,
+    file: require('./custom_modules/getWammed.js'),
+  },
+  roleAssignment: {
+    enabled: config.roleAssignment,
+    file: require('./custom_modules/roleAssignment.js'),
+  },
+};
 
-const client = new Discord.Client();
 const { token } = config;
-const notifyMode = config.notify;
-const getRoleIDsMode = config.getRoleIDs;
-const antiWordMode = config.antiWord;
-const autoRolesMode = config.autoRoles;
-const channelRenamingMode = config.channeRenaming;
-const channelSortingMode = config.channelSorting;
-const unusedChannelsMode = config.unusedChannels;
-const getWammedMode = config.getWammed;
-const roleAssignmentMode = config.roleAssignment;
 
-
-const connectBot = async () => {
+const connectBot = async (client) => {
   try {
     logger.log('Attempting to connect.', path);
     await client.login(token);
@@ -47,53 +67,25 @@ const connectBot = async () => {
   } catch (err) {
     connectBot();
   }
-}
+};
 
-client.once('ready', () => {
+const botClient = new Discord.Client();
+connectBot(botClient);
 
-  if (notifyMode === 1) {
-    console.log('Loaded notify');
-    notify.loadModule(client);
-  }
-  if (getRoleIDsMode === 1) {
-    console.log('Loaded getRoleIDs');
-    getRoleIDs.loadModule(client);
-  }
-  if (antiWordMode === 1) {
-    console.log('Loaded antiWord');
-    antiWord.loadModule(client);
-  }
-  if (autoRolesMode === 1) {
-    console.log('Loaded autoRoles');
-    autoRoles.loadModule(client);
-  }
-  if (channelRenamingMode === 1) {
-    console.log('Loaded channelRenaming');
-    channelRenaming.loadModule(client);
-  }
-  if (channelSortingMode === 1) {
-    console.log('Loaded channelSorting');
-    channelSorting.loadModule(client);
-  }
-  if (unusedChannelsMode === 1) {
-    console.log('Loaded unusedChannels');
-    unusedChannels.loadModule(client);
-  }
-  if (getWammedMode === 1) {
-    console.log('Loaded getWammed');
-    getWammed.loadModule(client);
-  }
-  if (roleAssignmentMode === 1) {
-    console.log('Loaded roleAssignment');
-    roleAssignment.loadModule(client);
-  }
+botClient.once('ready', () => {
+  _.forEach(modules, (value) => {
+    if (value.enabled) {
+      value.file.loadModule(botClient);
+    }
+  });
 });
 
+botClient.on('error', error => logger.log(error, path));
 
 process.on('SIGINT', () => {
   console.log('Received SIGINT... Shutting down');
   logger.log('Received SIGINT... Shutting down', path);
-  channelRenaming.exit(client);
+  modules.channelRenaming.file.exit(botClient);
   setTimeout(() => {
     process.exit(0);
   }, 3000);
@@ -102,6 +94,5 @@ process.on('SIGINT', () => {
 process.on('uncaughtException', async (error) => {
   console.log(`Caught exception: ${error}`);
   logger.log(error, path);
-  await client.destroy();
-  connectBot();
+  process.exit(0);
 });
